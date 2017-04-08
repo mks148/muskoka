71d8bb7453058622fec85bb243ad8116e64ba8a8
mks148 <manoj.sharma@mygeorgian.ca>
2017-04-08 16:37:46 -0400

    parentspage

--- models/myclient.js
+++ models/myclient.js
@@ -3,31 +3,36 @@ var mongoose= require('mongoose');
 
 // my clientSchema to display or add  data in table according to this
 var clientSchema = new mongoose.Schema({
-	firstname: {
+	parentname: {
 		type: String,
 		required:'Name is required'
 	},
-lastname:{
+parentlastname:{
 	type:String,
 	required:'LastName is required'
 },
+phone:{
+	type:String,
+	required:'Phone is required'
+},
+campername:{
+	type:String,
+	required:'campername is required'
+},
+notes:{
+	type:String,
+	required:'notes is required'
+},
+medical:{
+	type:String,
+	required:'medical is required'
+},
+code:{
+	type:Number,
+	required:'code is required'
+}
 
-    starttime:{
-        type:Number,
-        required:'Start Date is required'
-    },
-    endtime:{
-        type:Number,
-        required:'Month is required'
-    },
-    date:{
-        type:Number,
-        required:'End Date is required'
-    },
-    parentname:{
-        type:String,
-        required:'End Month is required'
-    }
+
 });
 
 module.exports = mongoose.model('Client', clientSchema);
